be48524dea2c958c402888c15e4139e345c80d81
ueki5 <ueki005@gmail.com>
2019-08-25 23:20:29 +0900

    exe化

---
+++ WebScraping/build.bat
@@ -0,0 +1,8 @@
+pyinstaller CreateUser.py --onefile
+pyinstaller CreateWorkPackage.py --onefile
+pyinstaller DeleteUser.py --onefile
+pyinstaller DeleteWorkPackage.py --onefile
+pyinstaller GetUser.py --onefile
+pyinstaller GetWorkPackage.py --onefile
+pyinstaller PatchUser.py --onefile
+pyinstaller PatchWorkPackage.py --onefile
