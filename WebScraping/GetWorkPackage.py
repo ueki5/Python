572f67a7fa6c287e7977ad8f5a59060bf56353e8
ueki5 <ueki005@gmail.com>
2019-08-25 10:54:10 +0900

    WPが登録できるようになりました

--- WebScraping/GetWorkPackage.py
+++ WebScraping/GetWorkPackage.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 userid='apikey'
 passwd='34739f806ab1b3caa301a77871a35600c1470ac8b13187bdb8b0c5fa1cad0404'
 url='http://192.168.99.100:30080/api/v3/work_packages/'
-workpkg_id='42'
+workpkg_id='44'
 html_doc = requests.get(url + workpkg_id, headers={'Content-Type': 'application/hal+json'}, auth=(userid, passwd)).text
 soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoup
 print(soup.prettify())
