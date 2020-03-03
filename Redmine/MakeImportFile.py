a906e83c38747f2d4ab03651cbeac5f2596b3720
ueki5 <ueki005@gmail.com>
2020-03-03 15:32:17 +0900

    定義体名称整理＆時間情報追加

--- Redmine/MakeImportFile.py
+++ Redmine/MakeImportFile.py
@@ -14,63 +14,31 @@ from redminelib import Redmine
 ###################################################
 cfgpath = ""
 csvtktpath = ""
-sjistktpath = ""
-impshpath = ""
-delshpath = ""
-
 # CSV書き込み項目
 csvfmt = {
     "TicketNumber": 0,
-    "Title": 1,
-    "Priority": 3,
-    "State": 4,
+    "subject": 1,
+    "priority": 3,
+    "status": 4,
     "Type": 5,
     "CustomerID": 7,
     "CustomerUser": 8,
-    "Owner": 9,
-    "Responsible": 10,
-    "CreateTime": 11,
-    "DynamicField_ITSMDueDate": 12,
-    "DynamicField_ITSMImpact": 13,
-    "DynamicField_IncidentType": 14,
-    "DynamicField_TaiouKousuu": 15,
-    "DynamicField_XITSMCloseDate": 16,
-    "DynamicField_XITSMGeneralTextarea1": 17,
-    "DynamicField_XITSMGeneralTextarea3": 18,
-    "DynamicField_ProjectSpecific": 19
+    "assigned_to": 9,
+    "start_date": 10,
+    "due_date": 11,
+    "DynamicField_IncidentType": 12,
+    "DynamicField_TaiouKousuu": 13,
+    "DynamicField_XITSMCloseDate": 14,
+    "DynamicField_XITSMGeneralTextarea1": 15,
+    "DynamicField_XITSMGeneralTextarea3": 16,
+    "DynamicField_ProjectSpecific": 17
 }
 #################################################
 # 関数
 ###################################################
 # 26進変換
-n_ary = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z"
-]
+n_ary=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
 # Excel列指定変換ヘルパー関数
 def _idxToA1(unit, ary, inp):
     nextAry = ary+[n_ary[inp%unit]]
@@ -107,6 +75,18 @@ def idxToRow(idx):
 # Excel行指定逆変換
 def rowToIdx(row):
     return row-1
+# 行列指定方法変換
+def cnvColRowFormula(fnRow, fnCol):
+    # 開始行
+    prjinf["ticketinfo"]["startrow"] = fnRow(prjinf["ticketinfo"]["startrow"])
+    # コラム（列）位置
+    for key, val in prjinf["ticketinfo"]["format"].items():
+        if key == "DynamicField_ProjectSpecific":
+            # 複数項目を保持している為、子要素に対して変換を行う
+            for key2, val2 in prjinf["ticketinfo"]["format"]["DynamicField_ProjectSpecific"].items():
+                prjinf["ticketinfo"]["format"]["DynamicField_ProjectSpecific"][key2] = fnCol(val2)
+        else:
+            prjinf["ticketinfo"]["format"][key] = fnCol(val)
 # 値の有効判定
 def isvalidval(val):
     return val is not None and val==val # None,nanは無効値
@@ -121,10 +101,9 @@ def shrinkclm(lst):
             # 単項目の場合
             dic[key] = lst[val]
         else:
-            # 複合項目の場合
+            # 複合項目の場合、JSON風の形式で詰め込む
             clmval = ""
             for subkey, subval in val.items():
-                # カンマの後にスペースが必要なので、注意（そうしないと画面上で複数項目に区切られません）
                 clmval += ", " if len(clmval) > 0 else ""
                 clmval += subkey + ":"
                 clmval += conv2str(lst[subval])
@@ -137,6 +116,10 @@ def conv2str(val):
     return ("{0:%Y/%m/%d %H:%M:%S}".format(val) if isinstance(val, datetime.date) 
             else "{0:%H:%M:%S}".format(val) if isinstance(val, datetime.time) 
             else str(val) if isvalidval(val) 
+            else "")
+# 日付変換
+def conv2date(str):
+    return (datetime.datetime.strptime(str, "%Y/%m/%d %H:%M:%S").date() if len(str) == 19 
             else "")
 # 書き出し用レコード作成
 def conv2csvfmt(srcdic):
@@ -165,17 +148,56 @@ def cnvval(dic, val):
 # レコード変換
 def cnvrec(rec):
     rec["TicketNumber"] = prjinf["TicketNumber"]["Prefix"] + rec["TicketNumber"].zfill(prjinf["TicketNumber"]["Length"])
-    rec["Priority"] = cnvval(prjinf["mstPriority"], rec["Priority"])
-    rec["State"] = cnvval(prjinf["mstState"], rec["State"])
-    rec["Type"] = "インシデント"
+    rec["priority"] = cnvval(prjinf["mstPriority"], rec["priority"])
+    rec["status"] = cnvval(prjinf["mstStatus"], rec["status"])
     rec["CustomerID"] = prjinf["CustomerID"]
     rec["CustomerUser"] = cnvval(prjinf["mstCustomerUser"], rec["CustomerUser"])
-    rec["Owner"] = cnvval(prjinf["mstUser"], rec["Owner"])
-    rec["Responsible"] = cnvval(prjinf["mstUser"], rec["Responsible"])
+    rec["assigned_to"] = cnvval(prjinf["mstUser"], rec["assigned_to"])
     rec["DynamicField_IncidentType"] = cnvval(prjinf["mstIncidentType"], rec["DynamicField_IncidentType"])
     for key,val in prjinf["ticketinfo"]["replaceclm"].items():
         rec[key] = re.sub(val["regexp"],val["repval"], rec[key])
     return rec
+# イシュー登録
+def updIssue(rec):
+    issue = getIssue(rec["TicketNumber"])
+    # 既存イシューの場合、作業時間、コメントの更新
+    if issue.id != 0:
+        # 作業時間の更新
+        updTimeEntry(issue.id)
+        # コメントの更新
+        updNotes(issue.id)
+    issue.project_id = project.id
+    issue.subject = rec["subject"]
+    issue.tracker_id = 3     #サービス固定
+    issue.status_id = 1      #ステータス
+    issue.priority_id = 1    #優先度
+    issue.assigned_to_id = 1 #担当者のID
+    issue.start_date = conv2date(rec["start_date"]) #開始日
+    issue.due_date = conv2date(rec["due_date"])   #期日
+    issue.estimated_hours = 4   # 予想工数
+    issue.custom_fields = [{'id': 7, 'value': rec["TicketNumber"]}]
+    issue.save()
+# イシュー取得
+def getIssue(external_key):
+    issues = redmine.issue.filter(project_id = project.id, cf_7 = external_key)
+    return issues[0] if len(issues) > 0 else redmine.issue.new()
+# 作業時間更新（暫定コード）
+def updTimeEntry(issue_id):
+    time_entries = redmine.time_entry.filter(project_id = project.id, issue_id = issue_id)
+    for time_entry in time_entries:
+        time_entry.spent_on = datetime.date(2014, 1, 31)
+        time_entry.hours = 99
+        time_entry.activity_id = 10
+        time_entry.comments = 'hello!'
+        time_entry.save()
+# コメント更新（delete,updateできないので追加のみ）（暫定コード）
+def updNotes(issue_id):
+    time_entries = redmine.time_entry.filter(project_id = project.id, issue_id = issue_id)
+    for time_entry in time_entries:
+        time_entry.spent_on = datetime.date(2014, 1, 31)
+        time_entry.hours = 99
+        time_entry.activity_id = 10
+        time_entry.comments = 'hello!'
 ###################################################
 # メイン処理
 ###################################################
@@ -185,9 +207,6 @@ parser.add_argument("projectid")
 args = parser.parse_args()
 cfgpath = args.projectid + ".json"
 csvtktpath = args.projectid + "_tickets.csv"
-sjistktpath = args.projectid + "_tickets_sjis.csv"
-impshpath = args.projectid + "_imp_all.sh"
-delshpath = args.projectid + "_del_all.sh"
 
 # 設定ファイル存在チェック
 if not(os.path.isfile(cfgpath)):
@@ -198,18 +217,14 @@ if not(os.path.isfile(cfgpath)):
 with open(cfgpath, encoding="utf-8", mode="r") as jsnfile:
     prjinf = json.load(jsnfile)
 #####################################################
-# 機械にわかりやすい用、行指定、列指定をExcel基準から変換
+# 基本項目
 #####################################################
-# 開始行
-prjinf["ticketinfo"]["startrow"] = rowToIdx(prjinf["ticketinfo"]["startrow"])
-# コラム（列）位置
-for key, val in prjinf["ticketinfo"]["format"].items():
-    if key == "DynamicField_ProjectSpecific":
-        # 複数項目を保持している為、子要素に対して変換を行う
-        for key2, val2 in prjinf["ticketinfo"]["format"]["DynamicField_ProjectSpecific"].items():
-            prjinf["ticketinfo"]["format"]["DynamicField_ProjectSpecific"][key2] = a1ToIdx(val2)
-    else:
-        prjinf["ticketinfo"]["format"][key] = a1ToIdx(val)
+apiUrl = prjinf["apiUrl"]
+apiKey = prjinf["apiKey"]
+#####################################################
+# 行列指定方法変換（Excel方式からインデックス方式へ）
+#####################################################
+cnvColRowFormula(rowToIdx, a1ToIdx)
 # EXCELファイル読み込み
 exlfile = pd.read_excel(prjinf["bookname"], sheet_name=prjinf["ticketinfo"]["sheetname"], header=None)
 # 有効行のみ、必要項目だけを抜き出す
@@ -217,38 +232,27 @@ exlrecs = [conv2csvfmt(shrinkclm(exlrec))
             for idx, exlrec in enumerate(exlfile.values) 
             if isvalidrec(idx, exlrec)]
 # 優先度マッピング追加
-addmst(prjinf, "mstPriority", uniquedic(["Priority"],exlrecs))
+addmst(prjinf, "mstPriority", uniquedic(["priority"],exlrecs))
 # 状態マッピング追加
-addmst(prjinf, "mstState", uniquedic(["State"],exlrecs))
+addmst(prjinf, "mstStatus", uniquedic(["status"],exlrecs))
 # 利用者マッピング追加
 addmst(prjinf, "mstCustomerUser", uniquedic(["CustomerUser"],exlrecs))
 # 運用者マッピング追加
-addmst(prjinf, "mstUser", uniquedic(["Owner", "Responsible"],exlrecs))
+addmst(prjinf, "mstUser", uniquedic(["assigned_to"],exlrecs))
 # インシデント分類マッピング追加
 addmst(prjinf, "mstIncidentType", uniquedic(["DynamicField_IncidentType"],exlrecs))
 #####################################################
-# 人間にわかりやすいよう、行指定、列指定をExcel基準に変換
+# 行列指定方法戻し（インデックス方式からExcel方式へ）
 #####################################################
-# 開始行
-prjinf["ticketinfo"]["startrow"] = idxToRow(prjinf["ticketinfo"]["startrow"])
-# コラム（列）位置
-for key, val in prjinf["ticketinfo"]["format"].items():
-    if key == "DynamicField_ProjectSpecific":
-        # 複数項目を保持している為、子要素に対して変換を行う
-        for key2, val2 in prjinf["ticketinfo"]["format"]["DynamicField_ProjectSpecific"].items():
-            prjinf["ticketinfo"]["format"]["DynamicField_ProjectSpecific"][key2] = idxToA1(val2)
-    else:
-        prjinf["ticketinfo"]["format"][key] = idxToA1(val)
+cnvColRowFormula(idxToRow, idxToA1)
 # JSONファイル書き込み
 with open(cfgpath, encoding="utf-8", mode="w") as jsnfile:
     json.dump(prjinf, jsnfile, ensure_ascii=False, indent=4)
-# インポートシェル書き込み
-with open(impshpath, encoding="utf-8", newline='', mode='w') as impshfile:
-    impshfile.write("#!/bin/bash\n")
-    impshfile.write("perl /opt/FJSVbsmotrs/bin/otrs.ImportExport.pl -n 000005 -a import -i /root/import/{}\n".format(csvtktpath))
-# CSVファイル、削除シェル書き込み
-with open(csvtktpath, encoding="utf-8", newline='', mode='w') as csvtktfile, \
-     open(delshpath, encoding="utf-8", newline='', mode='w') as delshfile:
+# サーバ接続
+redmine = Redmine(apiUrl, key=apiKey)
+project = redmine.project.get(prjinf["CustomerID"])
+# CSVファイル書き込み
+with open(csvtktpath, encoding="utf-8", newline='', mode='w') as csvtktfile:
     csvwriter = csv.DictWriter(
                             csvtktfile,
                             fieldnames=list(csvfmt.keys()), 
@@ -257,19 +261,10 @@ with open(csvtktpath, encoding="utf-8", newline='', mode='w') as csvtktfile, \
                             lineterminator='\n', 
                             quoting=csv.QUOTE_NONNUMERIC)
     csvwriter.writeheader()
-    delshfile.write("#!/bin/bash\n")
     for rec in exlrecs:
         recw = cnvrec(rec)
         csvwriter.writerow(recw)
-        delshfile.write("perl /opt/FJSVbsmotrs/bin/otrs.TicketDelete.pl --TicketNumber {}\n".format(recw["TicketNumber"]))
+        updIssue(recw)
 print("インポート用のCSV（{}）が作成されました。\n".format(csvtktpath))
-# 確認用にSJIS変換
-try:
-    with open(csvtktpath, encoding="utf-8", newline='', mode='r') as csvtktfile, \
-        open(sjistktpath, newline='', mode='w') as sjisfile:
-        sjisfile.write(csvtktfile.read())
-except:
-    # 存在しない場合、メッセージ表示して終了
-    print("確認用のCSV（{}）の作成に失敗しました。\n".format(sjistktpath))
-else:
-    print("確認用のCSV（{}）が作成されました。\n".format(sjistktpath))
+# 終了メッセージ
+print("処理が完了しました\n")
